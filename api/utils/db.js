a4fffe98836f4d8ec85bbc0355d739c1b2a6a646
MdAmeerAzam <150573913+MdAmeerAzam@users.noreply.github.com>
2026-03-15 16:09:35 +0530

    Add files via upload

--- api/utils/db.js
+++ api/utils/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: "postgresql://postgres.ybnpnpisvalswxyjjfvx:Qzh3nc8S%40UQezjc@aws-0-ap-northeast-1.pooler.supabase.com:6543/postgres",
+    connectionString: "postgresql://postgres.ybnpnpisvalswxyjjfvx:Qzh3nc8S%40UQezjc@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres",
     ssl: { rejectUnauthorized: false }
 });
 
